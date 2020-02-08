377086ae6ef841787db6c783cc01aa8346773fab
DUDA-UA <tayboy@ukr.net>
2020-02-08 18:40:45 +0200

    New

--- dist/script/script.js
+++ dist/script/script.js
@@ -2,6 +2,6 @@ $('.friends__slider').slick();
 
 $(function () {
     $('.toogle__input').on('click', function () {
-        $('#menu:checked').toggle('menu--open');
+        $('#menu').toggle('menu--open');
     });
 });
